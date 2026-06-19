f65609cbd97f1462dfc14c0d375fb49a70a0a86e
Gabriel Torres <gabrielctorres38@gmail.com>
2026-06-19 16:18:42 -0300

    resolvendo erros part01

--- Assets/_Project/Scripts/Factory/TabRules.cs
+++ Assets/_Project/Scripts/Factory/TabRules.cs
@@ -10,10 +10,9 @@ public class TabRules
     public float mixedChance;
 
     public float sidelessChance; // Chance de adicionar um segundo acompanhamento extra ao pedido.  (tive que comentar pq essa gambiarra ficou cheio de coisa)
-    public float extraSideChance; // Chance de adicionar um segundo acompanhamento extra ao pedido.
     public bool varySideCategory;  // Se true,  a categoria do acompanhamento pode ser sorteada independente da categoria do principal
 
-    public TabRules(int minOrders = 1, int maxOrders = 4, int maxQuantity = 3, float veganChance = 0.5f, float foodOnlyChance = 0.4f, float drinkOnlyChance = 0.4f, float mixedChance = 0.2f, float sidelessChance = 0.1f, float extraSideChance = 0f, bool varySideCategory = false)
+    public TabRules(int minOrders = 1, int maxOrders = 1, int maxQuantity = 1, float veganChance = 0.5f, float foodOnlyChance = 0.4f, float drinkOnlyChance = 0.4f, float mixedChance = 0.2f, float sidelessChance = 0.1f, bool varySideCategory = false)
     {
         this.minOrders = minOrders;
         this.maxOrders = maxOrders;
@@ -23,7 +22,6 @@ public class TabRules
         this.drinkOnlyChance = drinkOnlyChance;
         this.mixedChance = mixedChance;
         this.sidelessChance = sidelessChance;
-        this.extraSideChance = extraSideChance;
         this.varySideCategory = varySideCategory;
     }
 }
